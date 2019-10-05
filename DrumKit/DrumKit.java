1fb03622e931b673f76447d8751b29c49dc74671
sviatoslav <svyatoslav.grinik@gmail.com>
2019-10-05 22:05:58 +0200

    master: added DrumKit

---
+++ DrumKit/DrumKit.java
@@ -0,0 +1,13 @@
+package DrumKit;
+
+class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+    void playSnare() {
+        System.out.println("Бах бах ба-бах!");
+    }
+    void playTopHat() {
+        System.out.println("Динь динь ди-динь!");
+    }
+}
+
