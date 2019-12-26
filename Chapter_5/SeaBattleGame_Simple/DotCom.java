c1989133efed95493d621d68fbbf229a68218ac0
sviatoslav <svyatoslav.grinik@gmail.com>
2019-12-26 01:22:13 +0100

    master: added Chapter_5/.../

---
+++ Chapter_5/SeaBattleGame_Simple/DotCom.java
@@ -0,0 +1,29 @@
+package SeaBattleGame_Simple;
+import java.util.ArrayList;
+
+public class DotCom {
+    int[] locationCells;
+    int numOfHits= 0;
+
+    public void setLocationCells(int[] locs){
+        locationCells = locs;
+    }
+    public String checkYourself(String stringGuess){
+        int guess = Integer.parseInt(stringGuess);
+        String result = "Miss";
+
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "Hit";
+                numOfHits++;
+
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "Kill";
+        }
+        System.out.println(result);
+        return result;
+    }
+}
