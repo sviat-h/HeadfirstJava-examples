fcb7b2c16969d2f9161fcfe093252c91ebf27adf
sviatoslav <svyatoslav.grinik@gmail.com>
2020-01-11 13:38:49 +0100

    master: added .../Duck/...

---
+++ Chapter_9/Duck/Duck.java
@@ -0,0 +1,13 @@
+package Duck;
+
+public class Duck {
+    int size;
+
+    public Duck(int duckSize) {
+        size = duckSize;
+    }
+
+    public Duck() {
+        size = 10;
+    }
+}
---
+++ Chapter_9/Duck/UseADuck.java
@@ -0,0 +1,9 @@
+package Duck;
+
+public class UseADuck {
+    public static void main(String[] args) {
+        Duck d = new Duck();
+
+        System.out.println("Size = "+ d.size);
+    }
+}
